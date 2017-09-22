e8c4c733835372c1d8dfff9007208116da332587
Xiaonan Zhang <zxnmail@gmail.com>
2017-09-22 16:10:05 +0100

    HelloWorld controller with Spring Boot

---
+++ src/main/java/com/neptunezxn/demo/spring/boot/Main.java
@@ -0,0 +1,14 @@
+package com.neptunezxn.demo.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Main.class, args);
+    }
+}
